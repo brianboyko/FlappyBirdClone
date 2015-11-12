c8b97f3457f8bb7d659e16c56d030641c59799a7
Brian Boyko <brian.boyko@gmail.com>
2015-11-12 06:41:57 -0600

    First commit

---
+++ desktop/src/com/brianboyko/flappy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.brianboyko.flappy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brianboyko.flappy.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = FlappyDemo.WIDTH;
+		config.height = FlappyDemo.HEIGHT;
+		config.title = FlappyDemo.TITLE;
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
